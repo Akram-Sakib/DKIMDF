a6c21f6ef126e43282ec87d1a53dc9730d60ca6f
akram-sakib <sayedakramsakib@gmail.com>
2023-12-16 21:48:22 +0600

    :fire: updated logo name

--- src/components/home/NewsAndEvents.tsx
+++ src/components/home/NewsAndEvents.tsx
@@ -36,8 +36,6 @@ const NewsAndEvents = () => {
       </h2>
       <div className="grid grid-cols-3 gap-10 mt-5 [&>*]:text-2xl">
         {
-          // Loop 6 times
-
           news.map((item, i) => (
             <div key={item.id} className="">
               <div className="relative h-96 w-full shadow-lg hover:shadow-2xl   transition duration-500 ease-in-out">
--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,16 +1,15 @@
-
 import { CalendarForm } from "./HeaderCalender";
 import Navbar from "./Navbar";
 
 const Header = () => {
   return (
     <>
-      <div className="flex justify-between items-center [&>*]:p-2 [&>*]:px-5 text-lg bg-[#9E5BBA] text-white border-b border-red-600">
-        <h2>Logo</h2>
-        <h2 className="basis-[80%] text-center">
+      <div className="flex justify-between items-center [&>*]:p-2 [&>*]:px-5 text-base bg-[#9E5BBA] text-white border-b border-red-600">
+        <h2 className="basis-[30%] text-green">Shamadhan Foundation</h2>
+        <h2 className="basis-[60%] text-center">
           Dynamic Krishok & Imam Muazzin Development Foundation
         </h2>
-        <div>
+        <div className="basis-[10%]">
           <CalendarForm />
         </div>
       </div>
