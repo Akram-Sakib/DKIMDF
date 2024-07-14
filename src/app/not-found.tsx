7556e3e5a5a024bf8c10fc8f3ef0f8fcded67905
Akram Hossain <74033438+Akram-Sakib@users.noreply.github.com>
2024-07-14 11:09:16 +0600

    :updated

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,38 +12,38 @@ export default function NotFound() {
       <span className="bg-gradient-to-b from-foreground to-transparent bg-clip-text text-[10rem] font-extrabold leading-none text-transparent">
         500
       </span>
-      <h2 className="my-2 font-heading text-2xl font-bold">
+      {/* <h2 className="my-2 font-heading text-2xl font-bold">
        Internal Server Error
-      </h2>
-{/*       <h2 className="my-2 font-heading text-2xl font-bold">
-        Something&apos;s missing
       </h2> */}
-{/*       <p>
+      <h2 className="my-2 font-heading text-2xl font-bold">
+        Something&apos;s missing
+      </h2>
+      <p>
         Sorry, the page you are looking for doesn&apos;t exist or has been
         moved.
-      </p> */}
-      <p>
-        If you are the author of this website, please contact your developer.
       </p>
-      // <div className="mt-8 flex justify-center gap-2">
-      //   <Button
-      //     onClick={() => router.push("/dashboard")}
-      //     variant="outline"
-      //     size="lg"
-      //   >
-      //     Back to Dashboard
-      //   </Button>
-      //   <Button onClick={() => router.back()} variant="default" size="lg">
-      //     Go back
-      //   </Button>
-      //   <Button
-      //     onClick={() => router.push("/")}
-      //     variant="outline"
-      //     size="lg"
-      //   >
-      //     Back to Homepage
-      //   </Button>
-      // </div>
+      {/* <p>
+        If you are the author of this website, please contact your developer.
+      </p> */}
+       <div className="mt-8 flex justify-center gap-2">
+        <Button
+          onClick={() => router.push("/dashboard")}
+          variant="outline"
+          size="lg"
+        >
+          Back to Dashboard
+        </Button>
+        <Button onClick={() => router.back()} variant="default" size="lg">
+          Go back
+        </Button>
+        <Button
+          onClick={() => router.push("/")}
+          variant="outline"
+          size="lg"
+        >
+          Back to Homepage
+        </Button>
+      </div>
     </div>
   );
 }
